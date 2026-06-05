40bf20f223f29140eb6c26db33ccc561d2328778
Remyster <50181792+Remyster@users.noreply.github.com>
2026-06-05 07:09:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,24 @@
-const CACHE = 'gymlog-v6';
+const CACHE = 'gymlog-v2';
+const FILES = [
+  '/Bonusbuurt/',
+  '/Bonusbuurt/index.html',
+  '/Bonusbuurt/manifest.json',
+  '/Bonusbuurt/icon-192.png',
+  '/Bonusbuurt/icon-512.png'
+];
 
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  if (!e.request.url.startsWith(self.location.origin)) return;
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      const net = fetch(e.request).then(res => {
-        if (res.ok) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      });
-      return cached || net;
-    })
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
